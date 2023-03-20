f1d78dcbfceb0605bb1859ec8bd7eb19d53f276f
Victoria Kalenik <vk781@hotmail.com>
2023-03-20 21:51:35 +0000

    Add audio files

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// def sound variables
+const av = document.querySelector(`audio[data-composition='vivaldi-1']`);
+ss.play();
+const compositions = [
+    ['Saint-Saens: The Dance of the Death', 'saint-saens-1'],
+    ['Franz Schubert:_Musical-moment_in-F-minor']
+]
