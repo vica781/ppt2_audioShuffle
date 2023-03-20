06eb45f2de1feea0c72566b3c763c0c33598ec57
Victoria Kalenik <vk781@hotmail.com>
2023-03-20 22:59:58 +0000

    Address issue with audio playing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 // def sound variables
-const av = document.querySelector(`audio[data-composition='vivaldi-1']`);
-ss.play();
-const compositions = [
-    ['Saint-Saens: The Dance of the Death', 'saint-saens-1'],
-    ['Franz Schubert:_Musical-moment_in-F-minor']
-]
+// const vivaldi = document.querySelector(`audio[data-composition='vivaldi-1']`);
+// vivaldi.play();
+
+const vivaldi = document.querySelector(`audio[data-composition='vivaldi-1']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  vivaldi.play();
+});
+
+
+
+// const compositions = [
+//     ['Saint-Saens: The Dance of the Death', 'saint-saens-1'],
+//     ['Franz Schubert:_Musical-moment_in-F-minor']
+// ]
