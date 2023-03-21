eadf88c91f6880788a6042b2914b78c4c5202cd9
Victoria Kalenik <vk781@hotmail.com>
2023-03-21 22:06:27 +0000

    Add compositions database to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,106 +1,22 @@
-// def sound variables
-const vivaldi_1 = document.querySelector(`audio[data-composition='vivaldi-1']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  vivaldi_1.play();
-});
-
-const vivaldi_2 = document.querySelector(`audio[data-composition='vivaldi-2']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  vivaldi_2.play();
-});
-
-const khachaturian_1 = document.querySelector(`audio[data-composition='khachaturian-1']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  khachaturian_1.play();
-});
-
-const mendelssohn_1 = document.querySelector(`audio[data-composition='mendelssohn-1']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  mendelssohn_1.play();
-});
-
-const bach_1 = document.querySelector(`audio[data-composition='bach-1']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  bach_1.play();
-});
-
-const bach_2 = document.querySelector(`audio[data-composition='bach-2']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  bach_2.play();
-});
-
-const beethoven_1 = document.querySelector(`audio[data-composition='beethoven-1']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  beethoven_1.play();
-});
-
-const beethoven_2 = document.querySelector(`audio[data-composition='beethoven-2']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  beethoven_2.play();
-});
-
-const beethoven_3 = document.querySelector(`audio[data-composition='beethoven-3']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  beethoven_3.play();
-});
-
-const paganini_1 = document.querySelector(`audio[data-composition='paganini-1']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  paganini_1.play();
-});
-
-const rimskyKorsakov_1 = document.querySelector(`audio[data-composition='rimsky-korsakov-1']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  rimskyKorsakov_1.play();
-});
-
-const tchaikovsky_1 = document.querySelector(`audio[data-composition='tchaikovsky-1']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  tchaikovsky_1.play();
-});
-
-const tchaikovsky_2 = document.querySelector(`audio[data-composition='tchaikovsky_2']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  tchaikovsky_2.play();
-});
-
-const tchaikovsky_3 = document.querySelector(`audio[data-composition='tchaikovsky-3']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  tchaikovsky_3.play();
-});
-
-const tchaikovsky_4 = document.querySelector(`audio[data-composition='tchaikovsky-4']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  tchaikovsky_4.play();
-});
-
-const tchaikovsky_5 = document.querySelector(`audio[data-composition='tchaikovsky-5']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  tchaikovsky_5.play();
-});
-
-const wagner_1 = document.querySelector(`audio[data-composition='wagner-1']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  wagner_1.play();
-});
-
-const mozart_1 = document.querySelector(`audio[data-composition='mozart-1']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  mozart_1.play();
-});
-
-const mozart_2 = document.querySelector(`audio[data-composition='mozart-2']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  mozart_2.play();
-});
-
-const mozart_3 = document.querySelector(`audio[data-composition='mozart-3']`);
-vivaldi.addEventListener('canplaythrough', () => {
-  mozart_3.play();
-});
-
-
-// const compositions = [
-//   ['Saint-Saens: The Dance of the Death', 'saint-saens-1'],
-//   ['Franz Schubert:_Musical-moment_in-F-minor']
-// ]
+const compositions = [
+  ['Antonio Vivaldi: The Seasons. Winter. December', 'vivaldi-1'],
+  ['Antonio Vivaldi: The Seasons. Summer.Thunderstorm', 'vivaldi-2'],
+  ['Aram Khachaturian: Gayane. Sabre Dance', 'khachaturian-1'],
+  ['Felix Mendelssohn: A Midsummer Nights Dream. Wedding March', 'mendelssohn-1'],
+  ['Johann Sebastian Bach: Suite N2. Badinerie', 'bach-1'],
+  ['Johann Sebastian Bach: Toccata and Fugue', 'bach-2'],
+  ['Ludwig van Beethoven: Sonata N14. Moonlight Sonata', 'beethoven-1'],
+  ['Ludwig van Beethoven: Symphony N5', 'beethoven-2'],
+  ['Ludwig van Beethoven: to Elise', 'beethoven-3'],
+  ['Niccolò Paganini: Caprice N24', 'paganini-1'],
+  ['Nikolai Rimsky-Korsakov: The Tale of Tsar Saltan. Flight of the bumblebee', 'rimsky-korsakov-1'],
+  ['Pyotr Ilyich Tchaikovsky: Concerto N1', 'tchaikovsky-1'],
+  ['Pyotr Ilyich Tchaikovsky: Nutcracker. Dance of the Sugar Plum Fairy', 'tchaikovsky-2'],
+  ['Pyotr Ilyich Tchaikovsky: Nutcracker. Waltz of the Flowers', 'tchaikovsky-3'],
+  ['Pyotr Ilyich Tchaikovsky: Swan Lake. Dance of the Little Swans', 'tchaikovsky-4'],
+  ['Pyotr Ilyich Tchaikovsky: Nutcracker. The Prince and the Sugar Plum Fairy. Adagio', 'tchaikovsky-5'],
+  ['Richard Wagner. Valkyrie. Flight of the Valkyries', 'wagner-1'],
+  ['Wolfgang Amadeus Mozart: Little Night Serenade. Serenade N13', 'mozart-1'],
+  ['Wolfgang Amadeus Mozart: Marriage of Figaro. Overture', 'mozart-2'],
+  ['Wolfgang Amadeus Mozart: Turkish March. Piano Sonata N11', 'mozart-3']
+]
