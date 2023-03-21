585d0b214f62813672b4b135260a7619909c7f69
Victoria Kalenik <vk781@hotmail.com>
2023-03-21 17:37:05 +0000

    Add audio to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,106 @@
 // def sound variables
-// const vivaldi = document.querySelector(`audio[data-composition='vivaldi-1']`);
-// vivaldi.play();
-
-const vivaldi = document.querySelector(`audio[data-composition='vivaldi-1']`);
+const vivaldi_1 = document.querySelector(`audio[data-composition='vivaldi-1']`);
 vivaldi.addEventListener('canplaythrough', () => {
-  vivaldi.play();
+  vivaldi_1.play();
+});
+
+const vivaldi_2 = document.querySelector(`audio[data-composition='vivaldi-2']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  vivaldi_2.play();
+});
+
+const khachaturian_1 = document.querySelector(`audio[data-composition='khachaturian-1']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  khachaturian_1.play();
+});
+
+const mendelssohn_1 = document.querySelector(`audio[data-composition='mendelssohn-1']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  mendelssohn_1.play();
+});
+
+const bach_1 = document.querySelector(`audio[data-composition='bach-1']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  bach_1.play();
+});
+
+const bach_2 = document.querySelector(`audio[data-composition='bach-2']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  bach_2.play();
+});
+
+const beethoven_1 = document.querySelector(`audio[data-composition='beethoven-1']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  beethoven_1.play();
+});
+
+const beethoven_2 = document.querySelector(`audio[data-composition='beethoven-2']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  beethoven_2.play();
+});
+
+const beethoven_3 = document.querySelector(`audio[data-composition='beethoven-3']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  beethoven_3.play();
+});
+
+const paganini_1 = document.querySelector(`audio[data-composition='paganini-1']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  paganini_1.play();
+});
+
+const rimskyKorsakov_1 = document.querySelector(`audio[data-composition='rimsky-korsakov-1']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  rimskyKorsakov_1.play();
+});
+
+const tchaikovsky_1 = document.querySelector(`audio[data-composition='tchaikovsky-1']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  tchaikovsky_1.play();
+});
+
+const tchaikovsky_2 = document.querySelector(`audio[data-composition='tchaikovsky_2']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  tchaikovsky_2.play();
+});
+
+const tchaikovsky_3 = document.querySelector(`audio[data-composition='tchaikovsky-3']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  tchaikovsky_3.play();
+});
+
+const tchaikovsky_4 = document.querySelector(`audio[data-composition='tchaikovsky-4']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  tchaikovsky_4.play();
+});
+
+const tchaikovsky_5 = document.querySelector(`audio[data-composition='tchaikovsky-5']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  tchaikovsky_5.play();
+});
+
+const wagner_1 = document.querySelector(`audio[data-composition='wagner-1']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  wagner_1.play();
+});
+
+const mozart_1 = document.querySelector(`audio[data-composition='mozart-1']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  mozart_1.play();
+});
+
+const mozart_2 = document.querySelector(`audio[data-composition='mozart-2']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  mozart_2.play();
+});
+
+const mozart_3 = document.querySelector(`audio[data-composition='mozart-3']`);
+vivaldi.addEventListener('canplaythrough', () => {
+  mozart_3.play();
 });
 
 
-
-// const compositions = [
-//     ['Saint-Saens: The Dance of the Death', 'saint-saens-1'],
-//     ['Franz Schubert:_Musical-moment_in-F-minor']
-// ]
+const compositions = [
+  ['Saint-Saens: The Dance of the Death', 'saint-saens-1'],
+  ['Franz Schubert:_Musical-moment_in-F-minor']
+]
