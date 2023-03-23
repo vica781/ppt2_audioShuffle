ccec2cb7fb1a338901e064cc3e9dd18aa43b1dd5
Victoria Kalenik <vk781@hotmail.com>
2023-03-23 18:23:15 +0000

    Add .quiz-field to HTML structure and code to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ const compositions = [
   ['Richard Wagner. Valkyrie. Flight of the Valkyries', 'wagner-1'],
   ['Wolfgang Amadeus Mozart: Little Night Serenade. Serenade N13', 'mozart-1'],
   ['Wolfgang Amadeus Mozart: Marriage of Figaro. Overture', 'mozart-2'],
-  ['Wolfgang Amadeus Mozart: Turkish March. Piano Sonata N11', 'mozart-3']
+  ['Wolfgang Amadeus Mozart: Turkish March. Piano Sonata N11', 'mozart-3'],
 ]
 
 let button = document.querySelector("button");
@@ -39,10 +39,26 @@ const question = document.querySelector(".question");
 const answers = document.querySelectorAll(".answers>*");
 const message = document.querySelector(".message");
 const questionsData = [
-  [" W.A. Mozart: Magic Flute", "mozart-1"],
-  ["J.S.Bach: Toccata in d minor BWV", "bach-1"],
-  ["Haydn: The Seasons", "haydn-1"],
-  ["A.Vivaldi: Magnificat", "vivaldi-1"],
+  ['Antonio Vivaldi: The Seasons. Winter. December', 'vivaldi-1'],
+  ['Antonio Vivaldi: The Seasons. Summer.Thunderstorm', 'vivaldi-2'],
+  ['Aram Khachaturian: Gayane. Sabre Dance', 'khachaturian-1'],
+  ['Felix Mendelssohn: A Midsummer Nights Dream. Wedding March', 'mendelssohn-1'],
+  ['Johann Sebastian Bach: Suite N2. Badinerie', 'bach-1'],
+  ['Johann Sebastian Bach: Toccata and Fugue', 'bach-2'],
+  ['Ludwig van Beethoven: Sonata N14. Moonlight Sonata', 'beethoven-1'],
+  ['Ludwig van Beethoven: Symphony N5', 'beethoven-2'],
+  ['Ludwig van Beethoven: to Elise', 'beethoven-3'],
+  ['Niccolò Paganini: Caprice N24', 'paganini-1'],
+  ['Nikolai Rimsky-Korsakov: The Tale of Tsar Saltan. Flight of the bumblebee', 'rimsky-korsakov-1'],
+  ['Pyotr Ilyich Tchaikovsky: Concerto N1', 'tchaikovsky-1'],
+  ['Pyotr Ilyich Tchaikovsky: Nutcracker. Dance of the Sugar Plum Fairy', 'tchaikovsky-2'],
+  ['Pyotr Ilyich Tchaikovsky: Nutcracker. Waltz of the Flowers', 'tchaikovsky-3'],
+  ['Pyotr Ilyich Tchaikovsky: Swan Lake. Dance of the Little Swans', 'tchaikovsky-4'],
+  ['Pyotr Ilyich Tchaikovsky: Nutcracker. The Prince and the Sugar Plum Fairy. Adagio', 'tchaikovsky-5'],
+  ['Richard Wagner. Valkyrie. Flight of the Valkyries', 'wagner-1'],
+  ['Wolfgang Amadeus Mozart: Little Night Serenade. Serenade N13', 'mozart-1'],
+  ['Wolfgang Amadeus Mozart: Marriage of Figaro. Overture', 'mozart-2'],
+  ['Wolfgang Amadeus Mozart: Turkish March. Piano Sonata N11', 'mozart-3'],
 ];
 
 let randomQuestion = Math.floor(Math.random() * 4);
@@ -74,3 +90,4 @@ for (let i = 0; i < 4; i++) {
     }, 1000);
   };
 }
+
